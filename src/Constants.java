baf3e8f7c66e7b62f36dcca97b7e66370b0eac03
Master-sum <285957109@qq.com>
2020-11-22 13:15:35 +0800

    Java常量与变量

---
+++ src/Constants.java
@@ -0,0 +1,13 @@
+public class Constants {
+
+    public static final int T = 4;//定义一个常量类
+
+    public static void main(String[] args){
+        final int S = 3;//这是个常量定义
+        double v = 5.6;
+        int t = 8;
+        System.out.println("one\n"+S*v);
+        System.out.println("two\n"+S*t);
+        System.out.println("san\n"+T*t);
+    }
+}
