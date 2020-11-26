bc2c2c09fd93d5ce2cc2987b58296c737e4c47be
Master-sum <285957109@qq.com>
2020-11-26 23:50:45 +0800

    创建对象

---
+++ src/oop_01.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class oop_01 {
+    public static void main(String[] args){
+//        构建时间对象
+        Date u =new Date();
+        String p = new Date().toString();
+        System.out.println(u);
+        System.out.println(p);
+    }
+
+}
+
